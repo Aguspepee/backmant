226a24ad937c9f597704f1b784804774eb6899af
Aguspepee <sanchezagustinezequiel@gmail.com>
2022-03-03 15:09:15 -0300

    Final del dia

--- controllers/sapController.js
+++ controllers/sapController.js
@@ -17,7 +17,21 @@ module.exports = {
         try{
             console.log("Id", req.body.userId)
             const sap = new sapsModel({
-                name: req.body.name
+                
+                    Orden: req.body.Orden,
+                    Equipo: req.body.Equipo,
+                    Ubicac_técnica:req.body.Ubicac_técnica,
+                    Texto_breve: req.body.Texto_breve,
+                    Inicio_program: req.body.Inicio_program,
+                    Fecha_ref: req.body.Fecha_ref,
+                    Grupo_planif: req.body.Grupo_planif,
+                    Clase_de_orden:req.body.Clase_de_orden,
+                    Cl_actividad_PM: req.body.Cl_actividad_PM,
+                    Status_usuario:req.body.Status_usuario,
+                    Pto_tbjo_resp: req.body.Pto_tbjo_resp,
+                    Trabajo_real: req.body.Trabajo_real,
+                    Operacion: req.bodyOperación
+                   
             })
             const document = await sap.save()
             console.log("se creo",document)
--- models/sapsModel.js
+++ models/sapsModel.js
@@ -1,8 +1,61 @@
 const mongoose = require("../bin/server");
+const errorMessage = require("../utils/errorMessage")
 
 //Schema
 const sapsShema = mongoose.Schema({
-  name: { type: String },
+  Orden: {
+    type:String,
+    required:[true, errorMessage.GENERAL.campo_obligatorio],
+    minlength:[8, errorMessage.GENERAL.min_length]
+  },
+  Equipo:{
+    type:String,
+    required:[true, errorMessage.GENERAL.campo_obligatorio],
+  },
+  Ubicac_técnica: {
+    type:String,
+    required:[true, errorMessage.GENERAL.campo_obligatorio],
+  },
+  Texto_breve: {
+    type:String,
+    required:[true, errorMessage.GENERAL.campo_obligatorio],
+  },
+  Inicio_program: {
+    type:String,
+    required:[true, errorMessage.GENERAL.campo_obligatorio],
+  },
+  Fecha_ref: {
+    type:String,
+    required:[true, errorMessage.GENERAL.campo_obligatorio],
+  },
+  Grupo_planif: {
+    type:String,
+    required:[true, errorMessage.GENERAL.campo_obligatorio],
+  },
+  Clase_de_orden: {
+    type:String,
+    required:[true, errorMessage.GENERAL.campo_obligatorio],
+  },
+  Cl_actividad_PM: {
+    type:String,
+    required:[true, errorMessage.GENERAL.campo_obligatorio],
+  },
+  Status_usuario: {
+    type:String,
+    required:[true, errorMessage.GENERAL.campo_obligatorio],
+  },
+  Pto_tbjo_resp:{
+    type:String,
+    required:[true, errorMessage.GENERAL.campo_obligatorio],
+  },
+  Trabajo_real: {
+    type:Number,
+    required:[true, errorMessage.GENERAL.campo_obligatorio],
+  },
+  Operacion: {
+    type:String,
+    required:[true, errorMessage.GENERAL.campo_obligatorio],
+  },
 });
 
 module.exports = mongoose.model("saps", sapsShema)
