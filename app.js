5ada61e2df9c56871961ab7fe543457133b2e03e
Aguspepee <sanchezagustinezequiel@gmail.com>
2022-03-13 16:52:26 -0300

    Cambios en cors

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 
-app.use(cors({ origin: "https://aguspepee.github.io", credentials: true }));
+app.use(cors({ origin: "https://aguspepee.github.io" }));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
